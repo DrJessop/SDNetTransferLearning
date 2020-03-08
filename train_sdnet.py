439d82329ffc72542d07cc2822d44649f7bca463
DrJessop <14ag89@queensu.ca>
2020-03-08 18:00:26 -0400

    Fixed file structure

--- train_sdnet.py
+++ train_sdnet.py
@@ -5,24 +5,14 @@ from models.SDNet import SDNet
 import torch
 from torch.utils.data import DataLoader, random_split
 from sdnet_train.SDNetTrain import train
-
-
-class GetConfigurations:
-
-    def __init__(self):
-        self.args = GetConfigurations.read_params()
-
-    @staticmethod
-    def read_params():
-        parser = argparse.ArgumentParser(description='')
-        parser.add_argument("--config", default='', help="Experiment configuration file", required=True)
-        return parser.parse_args()
-
+from utils.normalize import normalize
 
 if __name__ == "__main__":
-    config = GetConfigurations()
-    config = config.read_params().config
-    with open(GetConfigurations().read_params().config) as json_file:
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("--config", default='', help="Experiment configuration file", required=True)
+    config = parser.parse_args().config
+
+    with open(config) as json_file:
         conf = EasyDict(json.load(json_file)).params
 
     sdnet = SDNet(conf.num_a, conf.num_z, conf.device, conf.shape, conf.binary)
@@ -30,14 +20,17 @@ if __name__ == "__main__":
     p_images = torch.load(conf.loader_loc)
     p_images = p_images.to(conf.device)
 
+    for idx in range(p_images.shape[0]):
+        normalize(p_images[idx])
+
     if conf.model_src != "":
         sdnet.load_state_dict(torch.load(conf.model_src))
 
     n_train = int(conf.split_portion * len(p_images))
     train_set, val_set = random_split(p_images, lengths=(n_train, len(p_images) - n_train))
-    train_loader = DataLoader(train_set, batch_size=conf.batch_size, num_workers=conf.num_workers)
+    train_loader = DataLoader(train_set, batch_size=conf.batch_size, num_workers=conf.num_workers, shuffle=conf.shuffle)
     val_loader = DataLoader(val_set, batch_size=conf.batch_size, num_workers=conf.num_workers)
     data = train_loader, val_loader
 
-    train(data, sdnet, conf.epochs, conf.model_dest, conf.kl_weight, conf.show_every, conf.save_model, conf.lr)
+    train(data, sdnet, conf.epochs, conf.model_dest, val_set, conf.kl_weight, conf.show_every, conf.save_model, conf.lr)
 
